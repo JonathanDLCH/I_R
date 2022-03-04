7eeaa2d13647b42d686f16535a303ecea0f793bb
Jonathan De La Cruz Huerta <thefallensclasic@gmail.com>
2022-03-04 09:37:50 -0600

    funciones completas (reestructura)

--- laboratorio3/main.py
+++ laboratorio3/main.py
@@ -11,16 +11,14 @@ def delete_stopwords(dic_documentos):
     Funcion que nos permite estructurar el texto,
     quitamos simbolos y palabras vacias.
     """
+    print('StopWords...')
     for llave in dic_documentos:
         documento = dic_documentos[llave]
         re_punc = re.compile('[%s]'%re.escape(string.punctuation))
         documentoaux=""
         for word in documento:
-            if word not in re_punc.sub('',word):
-                continue
-            else:
+            if word in re_punc.sub('',word):
                 documentoaux=documentoaux+word
-
         dic_documentos[llave]=documentoaux
         
         #Palabras vacias en ingles
@@ -30,27 +28,44 @@ def delete_stopwords(dic_documentos):
         for word in documentoaux:
             if word not in stop_words:
                 documentoaux2=documentoaux2+' '+word
-
         dic_documentos[llave]=documentoaux2
+
     create_file('woutSW',dic_documentos)
 
-def truncamiento(words):
-    #PorterStemmer
-    print('\nPorterStemmer')
+def truncamiento(dic_documentos):
+    """
+    Funcion para truncar palabras con el algoritmo de PorterStemmer
+    """
+    print('\nPorterStemmer...')
     porter = PorterStemmer()
-    porterWords = [porter.stem(word) for word in words]
-    print(' '.join(porterWords))
-    create_file('truncamiento',porterWords)
+    for llave in dic_documentos:
+        words=""
+        documento = dic_documentos[llave]
+        documentoaux=documento.split(' ')
+        for word in documentoaux:
+            words+=' '+porter.stem(word)
+        dic_documentos[llave] = words
 
-def lematizacion(words):
+    create_file('truncamiento',dic_documentos)
+
+def lematizacion(dic_documentos):
+    """
+    Funcion para lematizar nuestro contenido
+    """
+    print('\nWordNetLemmatizer...')
     wnl = WordNetLemmatizer()
-    wnlwords = [wnl.lemmatize(word) for word in words]
-    print(' '.join(wnlwords))
-    create_file('lematizacion',wnlwords)
+    for llave in dic_documentos:
+        words=""
+        documento = dic_documentos[llave]
+        documentoaux=documento.split(' ')
+        for word in documentoaux:
+            words+=' '+wnl.lemmatize(word)
+        dic_documentos[llave] = words
+    create_file('lematizacion',dic_documentos)
 
 def create_file(name,dic):
     """
-    Funcion para crear un archivo de texto.
+    Funcion para crear un archivo de texto a partir de un diccionario.
     """
     fname = f'final-{name}.txt'
     file = open(fname,'at')
@@ -61,21 +76,17 @@ def create_file(name,dic):
             print('Error al escribir en el archivo',name)
     file.close()
 
+
+
 file = open('files-merged.txt','rt')
 dic_documentos = dict()
 content = file.read().lower()
 paragraph = content.split('\n')
-#print(paragraph[:100])
 i = 1
-for linea in paragraph[:20]:
+for linea in paragraph:
     dic_documentos[i]=paragraph[i-1]
     i+=1
 
-#print(dic_documentos)
-#print(dic_documentos.items)
-#print('len: ',len(dic_documentos))
-
-
-paragraph = delete_stopwords(dic_documentos)
-#truncamiento(paragraph)
-#lematizacion(paragraph)
+delete_stopwords(dic_documentos)
+truncamiento(dic_documentos)
+lematizacion(dic_documentos)
